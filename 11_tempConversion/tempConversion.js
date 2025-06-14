e4da139fef088df3b700b99db4b6b1fb44b5109a
xobett <cessarr862@gmail.com>
2025-06-13 20:01:02 -0500

    Add - Exercises 10 and 11 done

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,8 +1,24 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farenheitDegree) {
+  let celsiusResult = (farenheitDegree - 32) * 5/9;
+  if (!Number.isInteger(celsiusResult)) {
+    celsiusResult = roundedNumber(celsiusResult, 1);
+  }
+  return celsiusResult;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusDegree) {
+  let farenheitResult = (celsiusDegree * 9/5) + 32;
+    if (!Number.isInteger(farenheitResult)) {
+    farenheitResult = roundedNumber(farenheitResult, 1);
+  }
+  return farenheitResult;
 };
+
+function roundedNumber(number, decimalDigits = 0){
+  let multiplier = Math.pow(10, decimalDigits);
+  let finalResult = Math.round(number * multiplier) / multiplier;
+  return Number(finalResult.toFixed(decimalDigits));
+}
 
 // Do not edit below this line
 module.exports = {
